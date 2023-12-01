11870700d76e6d1a75657dd682c60597915ff02e
Crowdbotics <engineering@crowdbotics.com>
2023-12-01 10:40:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muscle-app-with-ac-131222",
+  url: "https://muscle-app-with-ac-131222.botics.co",
+  api: "https://muscle-app-with-ac-131222.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
